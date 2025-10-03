8f29d5d0cbba66053f1c50ff014606cdac791365
random() <randomhuman@diamondbyte.org>
2025-10-02 21:30:46 -0600

    logic but working (and some debug code i'll remove later)

--- main.py
+++ main.py
@@ -1,37 +1,48 @@
 def add(num1, num2):
-    if isinstance(num1, int):
-        if isinstance(num2, int):
-            added = num1 + num2
-            return added
-        else:
-            return "fail_num2_invalid"
-
-    else:
-        return "fail_num1_invalid"
+    try:
+        num1 = int(num1)
+        num2 = int(num2)
+        added = num1 + num2
+        return added
+    except ValueError:
+        return "fail_invalid"
 def subtract(num1, num2):
-    if isinstance(num1, int):
-        if isinstance(num2, int):
-            subtracted = num1 - num2
-            return subtracted
-        else:
-            return "fail_num2"
-    else:
-        return "fail_num1"
+    try:
+        num1 = int(num1)
+        num2 = int(num2)
+        subtracted = num1 - num2
+        return subtracted
+    except ValueError:
+        return "fail_invalid"
 def multiply(num1, num2):
-    if isinstance(num1, int):
-        if isinstance(num2, int):
-            multiplied = num1 * num2
-            return multiplied
-        else:
-            return "fail_num2"
-    else:
-        return "fail_num1"
+    try:
+        num1 = int(num1)
+        num2 = int(num2)
+        multiplied = num1 * num2
+        return multiplied
+    except ValueError:
+        return "fail_invalid"
 def divide(num1, num2):
-    if isinstance(num1, int):
-        if isinstance(num2, int):
+    try:
+        num1 = int(num1)
+        num2 = int(num2)
+        if num2 == 0:
+            return "fail_dividebyzero"
+        else:
             divided = num1 / num2
             return divided
-        else:
-            return "fail_num2"
-    else:
-        return "fail_num1"
+    except ValueError:
+        return "fail_invalid"
+# some debug stuff here
+operation = input("Select an operation. ")
+number01 = input("Insert first number. ")
+number02 = input("Insert second number. ")
+
+if operation == "add":
+    print(add(number01, number02))
+elif operation == "subtract":
+    print(subtract(number01, number02))
+elif operation == "multiply":
+    print(multiply(number01, number02))
+elif operation == "divide":
+    print(divide(number01, number02))
