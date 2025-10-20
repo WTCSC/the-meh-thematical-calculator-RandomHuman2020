40af2bc3e61e4e708870abb3273c3bf50a07498f
random <randomhuman783@yahoo.com>
2025-10-20 13:48:38 -0600

    move calculation functions to seperate python file

--- calc.py
+++ calc.py
@@ -1,39 +1,4 @@
-def add(num1, num2):
-    try:
-        num1 = int(num1)
-        num2 = int(num2)
-        added = num1 + num2
-        return added
-    except ValueError:
-        return "fail_invalid"
-def subtract(num1, num2):
-    try:
-        num1 = int(num1)
-        num2 = int(num2)
-        subtracted = num1 - num2
-        return subtracted
-    except ValueError:
-        return "fail_invalid"
-def multiply(num1, num2):
-    try:
-        num1 = int(num1)
-        num2 = int(num2)
-        multiplied = num1 * num2
-        return multiplied
-    except ValueError:
-        return "fail_invalid"
-def divide(num1, num2):
-    try:
-        num1 = int(num1)
-        num2 = int(num2)
-        if num2 == 0:
-            return "fail_dividebyzero"
-        else:
-            divided = num1 / num2
-            return divided
-    except ValueError:
-        return "fail_invalid"
-
+from calc_functs import *
 keepgoing = True
 print("I guess we're doing math now. \nGreat. \nWhat do you want to do?")
 
